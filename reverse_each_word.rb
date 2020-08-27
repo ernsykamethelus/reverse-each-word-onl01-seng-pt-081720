da8c1908b1b5381f13374e7b1fd2e23068c4e3d3
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-27 01:02:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
-string= "Hello there, and how are you?"
-
 def reverse_each_word(string)
-  puts "Hello there, and how are you?"
+
+string_array = string.split
+
+reversed_words = string_array.collect {|word| word.reverse}
+
+reversed_string = reversed_words.join(" ")
+
+return reversed_string
+
 end
-  
+
   
 
 
